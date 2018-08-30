cdb03e2226ae5cd730f387cf6be1eb6615ac1645
Natasha Cox <natashac1014@gmail.com>
2018-08-30 13:59:24 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Random_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Random random = new Random();
+        List<int> allNumbers = new List<int>();
+        for (int i = 1; i <= 20; i++)
+        {
+        
+            int rnum = random.Next(1,51);
+            allNumbers.Add(rnum);
+        }
+        List<int> sqrNum = new List<int>();
+        foreach (int num in allNumbers)
+        {
+            sqrNum.Add(num*num);
+        }
+        List<int> oddNum = new List<int>();
+        foreach (int num in sqrNum)
+        {
+            if ((num % 2) !=0)
+            {
+                oddNum.Add(num);
+            }
+            
+        }
+        Console.WriteLine(String.Join(", ", sqrNum));
+        Console.WriteLine(String.Join(", ", oddNum));
+        }
+    }
+}
